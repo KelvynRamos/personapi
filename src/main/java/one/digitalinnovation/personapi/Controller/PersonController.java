642a849b714dec676a6583b792fc4082274a1892
FrancaRamos <renatafranca75@gmail.com>
2021-11-01 15:19:08 -0300

    .

--- src/main/java/one/digitalinnovation/personapi/Controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/Controller/PersonController.java
@@ -1,15 +1,28 @@
 package one.digitalinnovation.personapi.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.MessageRespondeDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return "API Test!";
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageRespondeDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageRespondeDTO
+                .builder()
+                .message("Created person with ID" + savedPerson.getId())
+                .build();
     }
 }
